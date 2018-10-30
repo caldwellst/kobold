f4ba9ecd13a8cbe89a142dfbafe09be6304de588
caldwellst <caldwellst@gmail.com>
2018-10-30 16:15:15 +0300

    added data printing generics

--- R/kobold_class.R
+++ R/kobold_class.R
@@ -9,3 +9,18 @@ new_kobold <- function(data, cleaning, survey, choices) {
    class(object) <- c("kobold", class(object))
    return(object)
 }
+
+## Defining generics for kobold class objects
+
+## Load the actual data as a data frame if as.data.frame(kobold) is called
+
+as.data.frame.kobold <- function(df) {
+   as.data.frame(df$data)
+}
+
+## Load the actual data as a table if as_tibble(kobold) is called
+
+as_tibble.kobold <- function(df) {
+   as_tibble(df$data)
+}
+
