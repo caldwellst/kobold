4dfb9ae8b0c0e45cdd585b6a4a07d13939883c2d
caldwellst <caldwellst@gmail.com>
2018-12-06 08:13:31 +0300

    add_option

--- R/cleaning.R
+++ R/cleaning.R
@@ -3,8 +3,9 @@
 #' @importFrom dplyr mutate filter
 #' @importFrom rlang sym
 #' @importFrom glue glue
-#' @importFrom purrr pmap
+#' @importFrom purrr pmap map detect_index
 #' @importFrom stringr str_remove str_trim str_replace
+#' @importFrom R.utils insert
 kobold_cleaner <- function(kobold_file) {
 
    ## Function to remove surveys based on either a UUID or relevant logic
@@ -45,23 +46,52 @@ kobold_cleaner <- function(kobold_file) {
 
       if(!str_detect(c(filter(kobold_file$survey, name == q_name)$type), "^.*(select_multiple|select multiple)")) {
          change_response(q_name, value, uuid, relevant)
+         warning(glue("remove_option is removing the entire response {value} since {q_name} is a select_one question"))
       }
 
       else { ## Here we get the name of the select_multiple binary column to change the value for
          binary_name <- unique(names(kobold_file$data %>%
-                                   select(matches(paste0("(\\b", q_name, ")(.)(", value, "\\b)")))))
+                                   select(matches(paste0("(\\b", q_name, ")(.)(", value, "\\b)")),
+                                          -one_of(c(kobold_file$survey$name)))))
+         print(binary_name)
       }
 
       if(!is.na(uuid)) { ## making the changes if based on UUID
          kobold_file$data <<- mutate(kobold_file$data,
-                                     !!q_name := ifelse(X_uuid == uuid, select_mul_str_removal(!!sym(q_name), value), !!sym(q_name)),
+                                     !!q_name := ifelse(X_uuid == uuid, select_mul_str_removal(value, !!sym(q_name)), !!sym(q_name)),
                                      !!binary_name := ifelse(X_uuid == uuid, FALSE, !!sym(binary_name)))
       }
 
       else if(!is.na(relevant)) { ## making the changes if based on relevant logic
          kobold_file$data <<- mutate(kobold_file$data,
-                                     !!q_name := ifelse(!! convert_relevant(relevant), select_mul_str_removal(!!sym(q_name), value), !!sym(q_name)),
+                                     !!q_name := ifelse(!! convert_relevant(relevant), select_mul_str_removal(value, !!sym(q_name)), !!sym(q_name)),
                                      !!binary_name := ifelse(!! convert_relevant(relevant), FALSE, !!sym(binary_name)))
+      }
+   }
+
+   add_option <- function(q_name, value, uuid, relevant) {
+      if(!str_detect(c(filter(kobold_file$survey, name == q_name)$type), "^.*(select_multiple|select multiple)")) {
+         stop(glue("add_option failed to add {value} to {q_name} since it is not a select_multiple question"))
+      }
+
+      else { ## Generating the name of the binary column and also the order in which it should be inserted
+         binary_name <- unique(names(kobold_file$data %>%
+                                     select(matches(paste0("(\\b", q_name, ")(.)(", value, "\\b)")))))
+         l_name <- str_trim(str_extract(filter(kobold_file$survey, name == q_name)$type, "(?<=select_multiple).*$"))
+         choices <- filter(kobold_file$choices, list_name == l_name)$name
+         choice_loc <- match(value, choices)
+         }
+
+      if(!is.na(uuid)) {
+         kobold_file$data <<- mutate(kobold_file$data,
+                                     !!q_name := ifelse(X_uuid == uuid, select_mul_str_adder(value, !!sym(q_name), choices, choice_loc), !!sym(q_name)),
+                                     !!binary_name := ifelse(X_uuid == uuid, TRUE, !!sym(binary_name)))
+      }
+
+      else if(!is.na(relevant)) {
+         kobold_file$data <<- mutate(kobold_file$data,
+                                     !!q_name := ifelse(!! convert_relevant(relevant), select_mul_str_adder(value, !!sym(q_name), choices, choice_loc), !!sym(q_name)),
+                                     !!binary_name := ifelse(!! convert_relevant(relevant), TRUE, !!sym(binary_name)))
       }
    }
 
@@ -75,6 +105,10 @@ kobold_cleaner <- function(kobold_file) {
       }
       else if(type == "remove_option") {
          remove_option(q_name = name, value = value, uuid = uuid, relevant = relevant)
+      }
+
+      else if(type == "add_option") {
+         add_option(q_name = name, value = value, uuid = uuid, relevant = relevant)
       }
 
       else {
@@ -94,8 +128,20 @@ kobold_cleaner <- function(kobold_file) {
 ## Removes select_multiple value for cleaning
 #' @importFrom glue glue
 #' @importFrom stringr str_trim str_replace str_remove
+select_mul_str_removal <- function(value, column) {
+   option = glue("\\b{value}\\b")
+   str_trim(str_replace(str_remove(option, column), "  ", " "))
+}
 
-select_mul_str_removal <- function(string, option) {
-   option = glue("\\b{option}\\b")
-   str_trim(str_replace(str_remove(string, option), "  ", " "))
+## Adds select_multiple value for cleaning
+#' @importFrom glue glue
+#' @importFrom stringr str_split
+#' @importFrom purrr map
+select_mul_str_adder <- function(value, column, choices, choice_loc) {
+   split <- str_split(column, pattern = " ")
+   indices <- map(split, match, choices)
+   insert_index <- map(indices, function(x) which(order(c(choice_loc, x)) == 1))
+   exist_test <- map_lgl(indices, function(x) is.na(match(choice_loc, x)))
+   split <- ifelse(exist_test, map(split, function(x) append(x, value, after = choice_loc - 1)), split)
+   map_chr(split, str_c, collapse = " ")
 }
